746d57e6913b5879781fdc29ded5e1fc9270f7ab
Daniel Amaral <dfbamaral@gmail.com>
2024-03-11 20:40:42 +0000

    FE-2-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-//import './components/carousel'
+import SignUp from './components/SignUp'
 
 function App() {
   
 
   return (
     <>
-      
+    
+    <SignUp/>
+
     </>
   )
 }
