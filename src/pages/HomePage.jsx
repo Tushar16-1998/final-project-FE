2ee92332b9764f11e9df8ebc9cf4757d978294e1
Tushar16-1998 <tusharsunder2@gmail.com>
2024-03-14 19:23:30 +0000

    deployment2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -54,36 +54,38 @@ import image2 from '../assets/2.png';
 
 function HomePage() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen container mx-auto flex justify-between items-center">
-            <h1 className="text-3xl font-bold mb-8">Welcome to RentAssist</h1>
-            <div className="space-y-4">
-                <div className="carousel w-full">
-                    <div id="slide1" className="carousel-item relative w-full">
-                        <img src={image1} className="w-full" alt="Slide 1" style="width: 50%; height: auto;"/>
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide4" className="btn btn-circle">❮</a>
-                            <a href="#slide2" className="btn btn-circle">❯</a>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className='container mx-auto flex justify-between items-center'>
+                <h1 className="text-3xl font-bold mb-8">Welcome to RentAssist</h1>
+                <div className="space-y-4">
+                    <div className="carousel w-full">
+                        <div id="slide1" className="carousel-item relative w-full">
+                            <img src={image1} className="w-full" alt="Slide 1" style="width: 50%; height: auto;" />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#slide4" className="btn btn-circle">❮</a>
+                                <a href="#slide2" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                        <div id="slide2" className="carousel-item relative w-full">
+                            <img src={image2} className="w-full" alt="Slide 2" style="width: 50%; height: auto;" />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#slide1" className="btn btn-circle">❮</a>
+                                <a href="#slide3" className="btn btn-circle">❯</a>
+                            </div>
                         </div>
                     </div>
-                    <div id="slide2" className="carousel-item relative w-full">
-                        <img src={image2} className="w-full" alt="Slide 2" style="width: 50%; height: auto;"/>
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide1" className="btn btn-circle">❮</a>
-                            <a href="#slide3" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
+                    <Link
+                        to="/signup"
+                        className="btn btn-primary">
+                        Sign Up
+                    </Link>
+                    <div className="h-4"></div> {/* Adding some space */}
+                    <Link
+                        to="/login"
+                        className="btn btn-secondary">
+                        Login
+                    </Link>
                 </div>
-                <Link
-                    to="/signup"
-                    className="btn btn-primary">
-                    Sign Up
-                </Link>
-                <div className="h-4"></div> {/* Adding some space */}
-                <Link
-                    to="/login"
-                    className="btn btn-secondary">
-                    Login
-                </Link>
             </div>
         </div>
     );
