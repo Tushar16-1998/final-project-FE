947fed03c7b625b92700572122a775b35f7d8837
Tushar16-1998 <tusharsunder2@gmail.com>
2024-03-14 17:55:50 +0000

    deployment2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -103,14 +103,17 @@ function Navbar() {
                             <>
                                 <li><Link to="/login">Login</Link></li>
                                 <li><Link to="/signup">SignUp</Link></li>
+                                <li><Link to="/about">About</Link></li>
                             </>
                         )}
                         <li><Link to="/about">About</Link></li>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/signup">SignUp</Link></li>
                     </ul>
                 </div>
             </div>
             <div className="navbar-center">
-                <Link to="/" className="btn btn-ghost text-xl">RentAssist</Link>
+                <Link to="/mainpage" className="btn btn-ghost text-xl">RentAssist</Link>
             </div>
             <div className="navbar-end">
                 {isLoggedIn && (
