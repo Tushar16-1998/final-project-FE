693b43f2d96a395bd7b64ac6c5ccf7d3663e8e08
Tushar16-1998 <tusharsunder2@gmail.com>
2024-03-14 19:06:17 +0000

    deployment2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -49,7 +49,9 @@ function Navbar() {
     return (
         <nav className="navbar bg-gradient-to-b from-teal-500 to-blue-800 text-white min-h-screen">
             <div className="container mx-auto flex justify-between items-center">
-                <Link to="/mainpage" className="text-grey text-lg font-bold"><img src={icon} /></Link>
+                <Link to="/mainpage" className="text-grey text-lg font-bold">
+                    <img src={icon} alt="Logo" style={{ width: '50px', height: '50px' }} />
+                </Link>
                 <div className="flex items-center space-x-4">
                     <Link to="/about" className="btn btn-sm btn-primary">About</Link>
                     <Link to="/login" className="btn btn-sm btn-primary">Login</Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,11 +20,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import image1 from '../assets/1.png';
+import image2 from '../assets/2.png';
+
+
 function HomePage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-3xl font-bold mb-8">Welcome to RentAssist</h1>
             <div className="space-y-4">
+            <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src={image} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  </div>
                 <Link
                     to="/signup"
                     className="bg-blue-500 hover:white text-white font-semibold py-2 px-4 rounded inline-block">
@@ -38,6 +58,7 @@ function HomePage() {
                 </Link>
             </div>
         </div>
+        
     );
 }
 
