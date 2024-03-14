40e9cf686a68eebff64d7888f25d38b528f1713f
Tushar16-1998 <tusharsunder2@gmail.com>
2024-03-14 19:42:48 +0000

    deployment2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -59,7 +59,7 @@ function HomePage() {
             <div className="space-y-4">
                 <div className="carousel w-full">
                     <div id="slide1" className="carousel-item relative w-full">
-                        <img src={image1} className="w-full" alt="Slide 1" />
+                    <img src={image1} className="w-full" alt="Slide 1" style={{ width: '50%', height: 'auto' }} />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide4" className="btn btn-circle">❮</a>
                             <a href="#slide2" className="btn btn-circle">❯</a>
