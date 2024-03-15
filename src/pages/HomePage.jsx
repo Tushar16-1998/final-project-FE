23b3845a111b3f37d7933b17207a7434a6540489
Tushar16-1998 <tusharsunder2@gmail.com>
2024-03-15 09:26:47 +0000

    deployment2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -72,6 +72,20 @@ function HomePage() {
                             <a href="#slide3" className="btn btn-circle">❯</a>
                         </div>
                     </div>
+                    <div id="slide1" className="carousel-item relative w-full">
+                    <img src={image1} className="w-full" alt="Slide 3" style={{ width: '525px', height: '300px' }} />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide4" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+                    <img src={image2} className="w-full" alt="Slide 4" style={{ width: '525px', height: '300px' }} />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
                 </div>
                 <Link
                     to="/signup"
