e1e7c8d640fff382b064d70ae6b6e8cee208c72e
Tushar16-1998 <tusharsunder2@gmail.com>
2024-03-15 10:13:16 +0000

    deployment2

--- src/components/ListForm.jsx
+++ src/components/ListForm.jsx
@@ -95,7 +95,7 @@ const ListForm = () => {
         </thead>
         <tbody>
           {entries.map((entry) => (
-            <tr key={entry._id} className="border-b border-dark-blue">
+            <tr key={entry._id} className="border-b border-blue">
               <td className="py-2 px-4">{entry.destination}</td>
               <td className="py-2 px-4">{entry.passangers}</td>
               <td className="py-2 px-4">{entry.date1.substring(0, 10)}</td>
