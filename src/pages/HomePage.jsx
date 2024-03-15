e6f8ea2b0025d90395066b23573ad88046bf53e1
Tushar16-1998 <tusharsunder2@gmail.com>
2024-03-15 09:17:24 +0000

    deployment2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -59,14 +59,14 @@ function HomePage() {
             <div className="space-y-4">
                 <div className="carousel w-full">
                     <div id="slide1" className="carousel-item relative w-full">
-                    <img src={image1} className="w-full" alt="Slide 1" style={{ width: '262px', height: '150px' }} />
+                    <img src={image1} className="w-full" alt="Slide 1" style={{ width: '525px', height: '300px' }} />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide4" className="btn btn-circle">❮</a>
                             <a href="#slide2" className="btn btn-circle">❯</a>
                         </div>
                     </div>
                     <div id="slide2" className="carousel-item relative w-full">
-                    <img src={image2} className="w-full" alt="Slide 2" style={{ width: '50%', height: '50%' }} />
+                    <img src={image2} className="w-full" alt="Slide 2" style={{ width: '525px', height: '300px' }} />
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide1" className="btn btn-circle">❮</a>
                             <a href="#slide3" className="btn btn-circle">❯</a>
